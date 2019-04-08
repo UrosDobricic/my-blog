f950cca0da7b2f33bc4f0e41019c5fc6e9f3e459
UrosDobricic <dobricic.uros@icloud.com>
2019-04-08 20:11:03 +0200

    blog addons

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url('', views.post_list, name='post_list'),
-    url(r"(?P<pk>\d+)/$", views.post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
